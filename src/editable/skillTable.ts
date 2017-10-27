4dff68cd3dbbf3c17973c6bbba46402621962eb4
AdrienpMichel <adrienpmichel@gmail.com>
2017-10-27 16:48:41 +0200

    Adding Skill page

---
+++ src/editable/skillTable.ts
@@ -0,0 +1,10 @@
+import { Skill } from '../app/skills/skill';
+
+export const SkillTable: Skill[] = [
+  { name : 'Java', strength : 95, type: 'success' },
+  { name : 'Peoplecode', strength : 85, type: 'success'  },
+  { name : 'Android', strength : 85, type: 'success' },
+  { name : 'Angular io', strength : 60, type: 'warning'  },
+  { name : 'Cisco packet tracer', strength : 50, type: 'danger' },
+   { name : 'Anglais', strength : 95, type: 'success' },
+];
